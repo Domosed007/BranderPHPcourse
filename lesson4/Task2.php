2de9af5947e83c5be56db046b171b0af5de63a7f
Aleksandr <a.v.smirnov1992@gmail.com>
2022-02-15 12:00:37 +0200

    The task 2 was implemented using SWITCH

--- lesson4/Task2.php
+++ lesson4/Task2.php
@@ -3,12 +3,21 @@
 // возвращает день недели Если число не входит в заданый диапазон то возвращает сообщение об ошибке
 
 function day(int $num){
-    switch(!check($num)){
+    switch(check($num)){
         case 1:
-            return "Monday"
-            break;
+            return "Monday";
         case 2:
-            
+            return "Tuesday";
+        case 3:
+            return "Wednesday";
+        case 4:
+            return "Thursday";
+        case 5:
+            return "Friday";
+        case 6:
+            return "Saturday";
+        case 7:
+            return "Sunday";
     }
 }
 
@@ -19,3 +28,5 @@ function check(int $num){
     }
     return true;
 }
+
+echo(day(1));
