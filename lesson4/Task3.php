a2c68c6a66191c112628e77ed9a2060db4d91683
Aleksandr <a.v.smirnov1992@gmail.com>
2022-02-15 20:20:38 +0200

    Add recursia in Task3

--- lesson4/Task3.php
+++ lesson4/Task3.php
@@ -1,17 +1,29 @@
 <?php
 /*Написать функцию которая проверяет является ли число простым или нет(продвинутый уровень - использовтаь рекурсию)*/
 
-function is_simpleNumber(int $num){
-    if($num<=1){
-        return false;
-    }
-    for($i=2;$i<$num;$i++){
-        if($num%$i==0){
-            return false;
-        }
+// function is_simpleNumber(int $num){
+//     if($num<=1){
+//         return false;
+//     }
+//     for($i=2;$i<$num;$i++){
+//         if($num%$i==0){
+//             return false;
+//         }
+//     }
+
+//     return true;
+// }
+
+function is_simpleNumber(int $num,int $i=2){
+    if($i==$num){
+         return true;
     }
 
-    return true;
+    if($num%$i==0){
+        return false;
+    }
+
+    return is_simpleNumber($num,++$i);
 }
 
 var_dump(is_simpleNumber(6));
