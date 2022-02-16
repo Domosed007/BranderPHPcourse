23faa086b89555d5cd8b42bf510d73b84ef8584e
Aleksandr <a.v.smirnov1992@gmail.com>
2022-02-16 20:46:45 +0200

    Task1 use functions ord() and chr()

--- lesson5/Task1.php
+++ lesson5/Task1.php
@@ -6,9 +6,21 @@
 */
 
 function getStringCharUpper(string $str,  $ch){
+    if(strlen($ch)>1){
+        echo "Error! $ch isn`t char!It is string!";
+        return; 
+    }
+    
+    $intCh=ord($ch);
+    
+    if($intCh<97||$intCh>122){
+        echo "Error! $ch is not char!";
+        return;
+    }
+    
     for($i=0;$i<strlen($str);$i++){
         if($str[$i]==$ch){
-            $str[$i]=strtoupper($str[$i]);
+            $str[$i]=chr($intCh-32);
         }
     }
     return $str;
