96e95166f03a40c0189396ccf0b9f4e473c57d7f
Aleksandr <a.v.smirnov1992@gmail.com>
2022-02-16 20:21:50 +0200

    Add folder lesson5 and file Task1.php. Use function strtoupper()

---
+++ lesson5/Task1.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Написать функцию которая принимает на первым аргументом строку, а вторым символ.
+В случае если искомые символ присуствует в строке то заменить его встроке на такой 
+же но в верхнем регистре
+*/
+
+function getStringCharUpper(string $str,  $ch){
+    for($i=0;$i<strlen($str);$i++){
+        if($str[$i]==$ch){
+            $str[$i]=strtoupper($str[$i]);
+        }
+    }
+    return $str;
+}
+
+echo getStringCharUpper("apach",'p');
