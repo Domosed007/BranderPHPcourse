f687ea54f762ad82ff78b02d2d5fcc96c3d3629d
Aleksandr <a.v.smirnov1992@gmail.com>
2022-04-04 21:55:49 +0300

    Доработан файл index.php: теперь из массива  создаются массивы с данными для записи в файлы

--- lesson7/02/index.php
+++ lesson7/02/index.php
@@ -1,25 +1,54 @@
 <?php
-$file=fopen('Data.csv','r');
+    $students=[];
+    $excellentStudents=[];
+    $goodStudents=[];
+    $tripletsStudents=[];    
 
-$students=[];
+    $file=fopen('Data.csv','r');
 
-if($file){
-    while(($data=fgetcsv($file,1000,','))!==false){
+    if($file){
+        while(($data=fgetcsv($file,1000,','))!==false){
             $students[$data[0]][]=array_combine(['course','grade'],[$data[1],$data[2]]);
-    }    
+        }
+    }
+
+    fclose($file);
+
+    $retake=getArrayRetake($students);
+
+    foreach($students as $key=>$student){
+        switch(getMinGradeStudent($student)){
+            case 5:
+                $excellentStudents[]=[$key,''];
+                break;
+            case 4:
+                $goodStudents[]=[$key,''];
+                break;
+            case 3:
+                $tripletsStudents[]=[$key,''];
+                break;
+        }
+    }
+
+
+function getArrayRetake($students):array{
+    $arr=[];
+
+    foreach($students as $key=>$student){
+        foreach($student as $var){
+        if($var['grade']==2){
+            $arr[]=[$key,$var['course'],$var['grade']];
+        }
+    }
+    }
+
+    return $arr;
 }
-else echo "ERROR! File not open!".PHP_EOL;
 
-fclose($file);
-
-var_dump($students);
- 
-// function minGrade(array $students):{
-//     $min=0;
-
-    
-    
-// }
+function getMinGradeStudent(array $student):int{
+    $arrayGrade=array_column($student,'grade');
+    return min($arrayGrade);
+}
 
 function writeFile(array $data, string $nameFile){
     $puth=getcwd()."/Data/$nameFile";
@@ -27,9 +56,13 @@ function writeFile(array $data, string $nameFile){
     $file=fopen($puth,'a');
 
     foreach($data as $var){
-        fputcsv($file,$var);
+        fputcsv($file,$var,' ');
     }
 
     fclose($file);
 }
-?>
+
+writeFile($excellentStudents,'Отличники.csv');
+writeFile($goodStudents,'Хорошисты.csv');
+writeFile($tripletsStudents,'Троечники.csv');
+ writeFile($retake,'Пересдача.csv');
--- lesson7/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-$arr=[1,2,3];
-
-$new_arr=array_map(function($n){
-    return $n*$n*$n;
-},$arr);
-
-var_dump($new_arr);
