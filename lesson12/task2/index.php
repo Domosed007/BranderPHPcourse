2dd49d81c7962406ec9b89c55ce9a69363325eee
Aleksandr <a.v.smirnov1992@gmail.com>
2022-04-20 11:28:25 +0300

    Данные из БД выводятся на экран пользователя в виде таблицы

--- lesson12/task2/index.php
+++ lesson12/task2/index.php
@@ -1,8 +1,27 @@
 <?php
+$user = "user";
+$password = "user1234";
+$database = "list_products_db";
+$table = "products";
+
 try {
-    $conn = new PDO("mysql:host=localhost", "root", "vtldtlm007");
-    echo "Database connection";
-}
-catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+  $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
+  
+  $result=$db->query("SELECT*FROM $table");
+
+  echo "<table><tr><th>Id</th><th>Name</th><th>Price</th><th>Season</th></tr>";
+
+  while($row=$result->fetch()){
+      echo "<tr>";
+      echo "<td>".$row["Id"]."</td>";
+      echo "<td>".$row["Name"]."</td>";
+      echo "<td>".$row["Price"]."</td>";
+      echo "<td>".$row["Season"]."</td>";
+      echo "</tr>";
+  }
+  echo "</table>";
+
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
 }
